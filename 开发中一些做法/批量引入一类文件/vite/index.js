12afc69081e40817b04b50d4c10ae74be25da419
zy.min <>
2023-05-05 15:56:31 +0800

    补充:import.meta.glob 用法

--- 开发中一些做法/批量引入一类文件/vite/index.js
+++ 开发中一些做法/批量引入一类文件/vite/index.js
@@ -4,10 +4,21 @@
 // Vite v2
 import.meta.globEager;
 
-// 对于Vite〉v2，
+// 对于Vite〉v2
+//匹配某个文件夹下的所有 .js 文件：./path/*.js
+//匹配某个文件夹及其子文件夹下的所有 .vue 文件：./path/**/*.vue
+//排除某个文件夹下的某个文件：./path/!(*.md)
 import.meta.glob("*", { eager: true });
 
 //require 自动引入实现
 const moduleFiles = require.context("./modules", false, /\.ts$/);
 //import 自动引入实现
 const files = import.meta.globEager("./modules/*.ts");
+
+// https://blog.csdn.net/weixin_45172119/article/details/129233197
+const modules = import.meta.glob("./path/*.js");
+
+for (const path in modules) {
+  const module = await modules[path]();
+  // 在这里处理导入的模块
+}
