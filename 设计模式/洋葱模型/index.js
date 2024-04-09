40f186899789f0caf3a0718bf2800290ad8e3673
zy.min <zy.min@hexinfo.cn>
2024-04-09 13:50:14 +0800

    洋葱模型

---
+++ 设计模式/洋葱模型/index.js
@@ -0,0 +1,37 @@
+// 原理: 实战koa.js
+// let middleware = []
+// middleware.push((next) => {
+// 	console.log(0)
+// 	next()
+// 	console.log(3)
+// })
+// middleware.push((next) => {
+// 	console.log(1)
+// 	next()
+// 	console.log(1.1)
+// })
+// middleware.push(() => {
+//     console.log(2)
+// })
+
+// let fn = compose(middleware)
+// fn()
+// 打印01231.1
+
+function compose (middleware) {
+    return async function () {
+       let args = arguments
+       await dispatch(0)
+       function async dispatch (i) {
+          const fn = middleware[i]
+          if (!fn) return null
+          await fn(function next () {
+             dispatch(i + 1)
+          }, ...args)
+       }
+    }
+}
+
+export {compose}
+ 
+
